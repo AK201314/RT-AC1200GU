8e80315450591f67cf89b6e88573590c5aec63e8
AK201314 <88940210+AK201314@users.noreply.github.com>
2022-01-18 23:47:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,23 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+
+# 删除原来的argon
+rm -rf  package/lean/luci-theme-argon
+
+# 添加老竭力的argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# 修改默认主题为argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改密码为空，自定义名称
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+
+sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio0.ssid=RT-AC1200GU/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+sed -i '/set wireless.default_radio0.ssid=RT-AC1200GU/a\ set wireless.default_radio1.ssid=RT-AC1200GU_5G' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
